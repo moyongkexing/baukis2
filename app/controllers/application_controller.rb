9e56ae40f112e151a43bb4ee91990e6b38a517bf
moyongkexing <moyongkexing@gmail.com>
2021-01-18 02:03:33 +0000

    タイプミスを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     class IpAdressRejected < ActionController::ActionControllerError; end
 
     rescue_from StandardError, with: :rescue500
-    rescue_from Forbidden, width: :rescue403
-    rescue_from IpAdressRejected, width: :rescue403
+    rescue_from Forbidden, with: :rescue403
+    rescue_from IpAdressRejected, with: :rescue403
 
     
     private def set_layout
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
         @exception = e
         render "errors/forbidden", status: 403
     end
-    
+
     private def rescue500(e)
         render "errors/internal_server_error", status: 500
     end
