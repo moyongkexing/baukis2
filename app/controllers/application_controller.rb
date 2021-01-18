def441e3ff9b1459b560d16389045ca16bdbba3c
moyongkexing <moyongkexing@gmail.com>
2021-01-18 01:46:01 +0000

    403エラーの処理を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
     layout :set_layout
 
+    class Forbidden < ActionController::ActionControllerError; end
+    class IpAdressRejected < ActionController::ActionControllerError; end
+
     rescue_from StandardError, with: :rescue500
+    rescue_from Forbidden, width: :rescue403
+    rescue_from IpAdressRejected, width: :rescue403
 
     
     private def set_layout
@@ -12,6 +17,10 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    private def rescue403(e)
+        @exception = e
+        render "errors/forbidden", status: 403
+    end
     private def rescue500(e)
         render "errors/internal_server_error", status: 500
     end
