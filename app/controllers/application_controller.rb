8887d6530e3d973d7ef52a210e9a3346e2e1f739
moyongkexing <moyongkexing@gmail.com>
2021-01-18 05:17:06 +0000

    例外処理をリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,9 @@ class ApplicationController < ActionController::Base
 
     class Forbidden < ActionController::ActionControllerError; end
     class IpAdressRejected < ActionController::ActionControllerError; end
-
-    rescue_from StandardError, with: :rescue500
-    rescue_from Forbidden, with: :rescue403
-    rescue_from IpAdressRejected, with: :rescue403
-    rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+    
+    include ErrorHandlers
+    include ErrorHandlers if Rails.env.production?
     
     private def set_layout
         if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -15,18 +13,5 @@ class ApplicationController < ActionController::Base
         else
             "customer"
         end
-    end
-
-    private def rescue403(e)
-        @exception = e
-        render "errors/forbidden", status: 403
-    end
-
-    private def rescue404
-        render "errors/not_found", status: 404
-    end
-
-    private def rescue500(e)
-        render "errors/internal_server_error", status: 500
     end
 end
