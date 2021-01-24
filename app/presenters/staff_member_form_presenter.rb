ef14e1ca6f229a729094dbcd116fc203c527bfef
moyongkexing <moyongkexing@gmail.com>
2021-01-24 06:35:25 +0000

    スタイルシートの調整

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,6 +1,6 @@
 class StaffMemberFormPresenter < FormPresenter
     def full_name_block(name1, name2, label_text, options = {})
-        markup(:div) do |m|
+        markup(:div, class: "input-block") do |m|
             m << label(name1, label_text, class: options[:required] ? "required" : nil)
             m << text_field(name1, options)
             m << text_field(name2, options)
